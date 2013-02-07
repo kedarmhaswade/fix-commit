8ad92606e6ad7aa195d4e1ad4f5ff715ecf1ccd3
Krishna <god@mahabharata.in>
2013-02-07 06:31:08 -0800

    in a hurry, I should revisit this commit

--- len.c
+++ len.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include<string.h>
 int main() {
-  char *s = "You typed \"\"";
+  char *s = "You typed \"\"';
   printf("sizeof(s) = %ld\n", sizeof(s));
   printf("strlen(s) = %ld\n", strlen(s));
   printf("%%\n");
